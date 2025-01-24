5e6fd1521e43706d336f22482cbeb97a111f55a8
Mia Chen <miachen@mias-macbook-air-2.local>
2025-01-24 16:58:21 -0700

    finished the constructor for the subsystem

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+public final class Constants {
+    
+    public static final class SwerveConstants {
+        public static final double k_maxSpeed = Units.feetToMeters(18.9);
+    }
+}
--- src/main/java/frc/robot/subsystem/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystem/SwerveSubsystem.java
@@ -3,19 +3,31 @@ package frc.robot.subsystem;
 import java.io.File;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SwerveConstants;
 import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 public class SwerveSubsystem extends SubsystemBase {
-
     // Imports stuff from the JSON files
     File directory = new File(Filesystem.getDeployDirectory(),"swerve");
-        SwerveDrive swerveDrive;
-    
-        public SwerveSubsystem() {
-            
+    SwerveDrive swerveDrive;
+
+    public SwerveSubsystem() {
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+        try {
+            swerveDrive = new SwerveParser(directory).createSwerveDrive(SwerveConstants.k_maxSpeed);
+        }
+
+        catch (Exception e) {
+            throw new RuntimeException(e);
         }
         
-    
-}
-    
+        swerveDrive.setCosineCompensator(false);
 
+        
+    }
+        
+}
