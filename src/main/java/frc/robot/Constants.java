8781d76afd4402742e885f1fa2eef5c0ca3b9357
Makai Davis <makai@Makais-MacBook-Air.local>
2025-01-30 15:32:27 -0700

    binded the algae arm pickup and release buttons

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,10 +11,12 @@ public final class Constants {
 
     public static final class OperatorConstants {
         public static final int k_driverController = 0;
-        public static final double k_AlgaeArmRotationSpeed = 0.1;
-        public static final int k_algaeReleaseArmRotation = Button.kY.value;
-        public static final int k_algaePickupArmRotation = Button.kA.value;
-        //public static final int k_algaeRollerSpeed = 
+        public static final double k_AlgaeArmRotationSpeed = 0.1; //all speed variables will have to be adjusted later
+        public static final double k_algaeRollerSpeed = 0.1;
+        public static final int k_algaeReleaseArmButton = Button.kY.value;
+        public static final int k_algaePickupArmButton = Button.kA.value;
+        public static final int k_algaePickupRollerButton = 90; //this is going to be dpad POV 90degree
+        public static final int k_algaeReleaseRollerButton = 270; //this is going to be dpad POV 270degree
     }
 
     public static final class DriveConstants {
