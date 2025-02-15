881a8fb5bccde050b976c00a01c49ee9500b990d
Annika Raphael <annikaraphael@Annikas-Laptop.local>
2025-02-15 14:01:53 -0700

    started pathplannerin container and servesubsystem

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -3,10 +3,21 @@ package frc.robot.subsystems;
 import java.io.File;
 import java.util.function.DoubleSupplier;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.SwerveConstants;
 import swervelib.SwerveDrive;
 import swervelib.math.SwerveMath;
@@ -15,40 +26,93 @@ import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 public class SwerveSubsystem extends SubsystemBase {
-    // Imports stuff from the JSON files
-    File directory = new File(Filesystem.getDeployDirectory(),"swerve");
+
+    File directory = new File(Filesystem.getDeployDirectory(), "swerve");
     SwerveDrive swerveDrive;
+    RobotConfig config;
+
+    public Pose2d getPose() {
+        return swerveDrive.getPose();
+    }
 
     public SwerveSubsystem() {
         SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
 
         try {
             swerveDrive = new SwerveParser(directory).createSwerveDrive(SwerveConstants.k_maxSpeed);
-        }
-
-        catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
         swerveDrive.pushOffsetsToEncoders();
         swerveDrive.setCosineCompensator(false);
+
+        try {
+            config = RobotConfig.fromGUISettings();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        AutoBuilder.configure(
+            this::getPose, // Robot pose supplier
+            this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            this::setChassisSpeeds,
+            new PPHolonomicDriveController(
+                new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+            ),
+            config,
+            () -> {
+                // Boolean supplier that controls when the path will be mirrored for the red alliance
+                // This will flip the path being followed to the red side of the field.
+                // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+                var alliance = DriverStation.getAlliance();
+                if (alliance.isPresent()) {
+                    return alliance.get() == DriverStation.Alliance.Red;
+                }
+                return false;
+            },
+            this 
+        );
     }
-        
-    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX)
-    {
+
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
         return run(() -> {
-            // make the robot move
+           
             swerveDrive.drive(SwerveMath.scaleTranslation(new Translation2d(
                 translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
-                translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity() ),0.8),
-                Math.pow(angularRotationX.getAsDouble(),3) * swerveDrive.getMaximumChassisAngularVelocity(),
+                translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity()), 0.8),
+                Math.pow(angularRotationX.getAsDouble(), 3) * swerveDrive.getMaximumChassisAngularVelocity(),
                 true,
                 false);
         });
     }
 
-    @Override
-    public void periodic() {}
+    public void resetOdometry(Pose2d initialHolonomicPose) {
+        swerveDrive.resetOdometry(initialHolonomicPose);
+    }
+
+    public ChassisSpeeds getRobotVelocity() {
+        return swerveDrive.getRobotVelocity();
+    }
+
+    public void setChassisSpeeds(ChassisSpeeds chassisSpeeds) {
+        swerveDrive.setChassisSpeeds(chassisSpeeds);
+    }
+
+     // Get Auto Command
+    public Command getAutonomousCommand(String pathName) {
+        // Create a path following command using AutoBuilder. This will also trigger event markers.
+        return new PathPlannerAuto(pathName);
+    }
 
     @Override
-    public void simulationPeriodic() {}
+    public void periodic() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
+    }
 }
